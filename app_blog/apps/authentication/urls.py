157120c26c1b55df4590bbe6a75766102f5f1a15
JairCV <velaidescj@gmail.com>
2021-01-16 02:12:41 -0500

    agregando vista basada en clase

--- app_blog/apps/authentication/urls.py
+++ app_blog/apps/authentication/urls.py
@@ -1,9 +1,20 @@
 # #<!--2-->
-from django.urls import path
+# from django.urls import path
 # #<!--2-->
-from .views import registry
+# from .views import registry
 
 # #<!--2-->
+# urlpatterns = [
+#    path('registry/', registry, name="registry"),
+# ]
+
+# #<!--4-->
+from django.urls import path
+# #<!--4-->
+from .views import RecordView
+
+
+# #<!--4-->
 urlpatterns = [
-    path('registry/', registry, name="registry"),
+    path('registry/', RecordView.as_view(), name="registry"),
 ]
--- app_blog/apps/authentication/views.py
+++ app_blog/apps/authentication/views.py
@@ -1,8 +1,24 @@
 # #<!--2-->
-from django.shortcuts import render
-
+# from django.shortcuts import render
 
 # #<!--2-->
-def registry(request):
-    # #<!--2-->
-    return render(request, "registry.html")
+# def registry(request):
+#   #<!--2-->
+#   return render(request, "registry.html")
+from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm
+
+
+# #<!--4 view-based class-->
+class RecordView(View):
+    # noinspection PyMethodMayBeStatic
+    # used to display information <!--4-->
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, "registry.html", {"form": form})
+
+    # noinspection PyMethodMayBeStatic
+    # used to process information
+    def post(self, request):
+        pass
