f375026cae5202ff360a77cf1d1ed4d187b866fd
JairCV <velaidescj@gmail.com>
2021-01-18 09:10:44 -0500

    agregar sesiones flash con bootstrap4

--- app_blog/apps/blog/views.py
+++ app_blog/apps/blog/views.py
@@ -7,7 +7,13 @@ from django.http import HttpResponse
 # #<!--1-->
 # return HttpResponse("Hi world")
 
+# #<!--6-->
+from django.shortcuts import render
+
+
 # #<!--5-->
 def index(request):
     # #<!--5-->
-    return HttpResponse("Hi world")
+    # return HttpResponse("Hi world")
+    # #<!--6-->
+    return render(request, "blog.html")
--- app_blog/config/settings.py
+++ app_blog/config/settings.py
@@ -15,6 +15,8 @@ import os
 # #<!--1.1-->
 from .db import MYSQL
 from pathlib import Path
+#  # <!--6 import flash messages-->
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +134,13 @@ STATICFILES_DIRS = [
 
 # <!--5 use bootstrap4 for forms-->
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# <!--6 classes for bootstrap flash messages-->
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'debug',
+    message_constants.INFO: 'info',
+    message_constants.SUCCESS: 'success',
+    message_constants.WARNING: 'warning',
+    message_constants.ERROR: 'danger',
+
+}
